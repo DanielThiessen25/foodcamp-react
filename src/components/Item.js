3b7b3e7da0b3e67ce65c2e34587a4e2e0f368e69
Daniel Luiz <danielluizthiessen@gmail.com>
2021-05-09 19:16:22 -0300

    Terminei requisitos básicos e comecei a colocar o Router

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function Item(props){
     const [turn, setTurn] = React.useState(["", "off", 0]);
+    const [pelicula, setPelicula] = React.useState("pelicula ");
 
+    let classe = "item "+ turn[0];
     let qtd = 0;
     let menos = "menos " + turn[1];
     let mais = "mais "+ turn[1];
@@ -12,23 +14,41 @@ export default function Item(props){
     let preco = props.preco;
     let tipo = props.tipo;
 
-    function selecionar(){
-        if(turn[0] === "selecionado"){
-            setTurn(["", "off", 0]);
-            props.remover({nome, preco, tipo});
-        }
-        else{
+    
+
+    function aumentar (){
+            qtd = turn[2];
             qtd ++;
             setTurn(["selecionado", "", qtd]);
             props.adicionar({nome, preco, tipo});
-        }
-        
+            if(turn[2] == 0){
+                setPelicula("pelicula off");
+            }     
+
     }
 
-    let classe = "item "+ turn[0];
+    function diminuir (){
+        if(turn[2] != 0){
+            if(turn[2] === 1){
+                setTurn(["", "off", 0]);
+                props.remover({nome, preco, tipo});
+                setPelicula("pelicula");
+            }
+            else{
+                qtd = turn[2];
+                qtd --;
+                setTurn(["selecionado", "", qtd]);
+                props.remover({nome, preco, tipo});
+            }
+            
+        }
+    }
 
     return(
-        <div class={classe} onClick={selecionar}>
+        <div>
+        
+        <div class={classe} >
+        <div class={pelicula} onClick={aumentar}></div>
                     <img src={props.imagem} class="foto" />
                     <h2 class="nomex1">{props.nome}</h2>
                     <h3>{props.descricao}</h3>
@@ -38,11 +58,13 @@ export default function Item(props){
                             <div class="precox1">{props.preco}</div>
                         </div>
                         <div class="seletor">
-                        <div class={menos}>-</div>
+                        <div class={menos} onClick={diminuir}>-</div>
                         <div class={classeQuantidade}>{turn[2]}</div>
-                        <div class={mais}>+</div>
+                        <div class={mais} onClick={aumentar}>+</div>
                         </div>
                     </div>
                 </div>
+                
+            </div> 
     );
 }
--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -1,35 +1,50 @@
-export default function Rodape(props){
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+export default function Rodape(props) {
     let clicar = false;
     let classe = ["pedir", "Selecione os 3 itens para fechar o pedido"];
     let verificador = [false, false, false];
     let pedido = props.pedido;
-        for (let i = 0; i < pedido.length; i++) {
-            if (pedido[i].tipo === "prato") {
-                verificador[0] = true;
-            }
-            else if (pedido[i].tipo === "bebida") {
-                verificador[1] = true;
-            }
-            else if (pedido[i].tipo === "sobremesa") {
-                verificador[2] = true;
-            }
+    for (let i = 0; i < pedido.length; i++) {
+        if (pedido[i].tipo === "prato") {
+            verificador[0] = true;
         }
-        if (verificador[0] === true && verificador[1] === true && verificador[2] === true) {
-            classe = ["pedir pronto", "Tudo certo, pode pedir!"];
-            alert("PRONTO");
-            clicar = true;
+        else if (pedido[i].tipo === "bebida") {
+            verificador[1] = true;
         }
+        else if (pedido[i].tipo === "sobremesa") {
+            verificador[2] = true;
+        }
+    }
+    if (verificador[0] === true && verificador[1] === true && verificador[2] === true) {
+        classe = ["pedir pronto", "Tudo certo, pode pedir!"];
+        alert("PRONTO");
+        clicar = true;
+    }
 
-        function fechar(clicar){
-            if(clicar == true){
+    function fechar(clicar) {
+        if (clicar == true) {
             alert("FECHOU PEDIDO");
         }
-        }
+    }
 
-    return(
-        <div class="rodape">
-            <div class={classe[0]} onClick={()=>fechar(clicar)}> {classe[1]} </div>
-        </div>
-
+    return (
+        <Router>
+            <div class="rodape">
+                <Link to="/revisar"><div class={classe[0]} onClick={() => fechar(clicar)}> {classe[1]} </div></Link>
+                <Switch>
+                    <Route path="/revisar">
+                        <Revisar />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
